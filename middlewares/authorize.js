39c24f83e4dccf218fb49f4a4da6d9f467481e92
sabbir <shawon.mu11@gmail.com>
2022-05-22 00:22:39 +0600

    authorization finished

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,4 +1,7 @@
 // we are going to check if user is part of our system
+
+const jwt = require('jsonwebtoken')
+// this function authorizes 
 module.exports = function(req,res,next){
     //get request header first, because token is there.
     // It is front end developer's responsibility to pass token in header 
@@ -8,6 +11,17 @@ module.exports = function(req,res,next){
     let token = req.header('Authorization');
     if(!token) return res.status(401).send('Access denied.No token provided')
     token = token.split(" ")[1].trim() // getting <token>
+
+    // now we verify token 
+    try{
+        const verified_token_decoded = jwt.verify(token,process.env.JWT_SECRET_KEY)
+        // console.log(verified_token_decoded) //f we can see which users token is this 
+        req.user = verified_token_decoded; // we created user in req obj, and stored all users who accessed the data (where this middleware is used )
+        next(); // control passes to next one
+    }
+    catch(err){
+        return res.status(400).send('Invalid Token')
+    }
 
 }
 
--- routes/teacher.routes.js
+++ routes/teacher.routes.js
@@ -1,15 +1,16 @@
 const teacherRouter = require('express').Router()
 
 const {createTeacher,getTeacher,getSingleTeacher,updateTeacher,deleteTeacher} = require('../controllers/teacher.controllers')
+const authorize = require('../middlewares/authorize')
 
 teacherRouter.route('/')
-    .post(createTeacher)
+    .post([authorize],createTeacher)  // array is not mandatory 
     .get(getTeacher)
 
 teacherRouter.route('/:id')
     .get(getSingleTeacher)
-    .patch(updateTeacher)
-    .delete(deleteTeacher)
+    .patch([authorize],updateTeacher)
+    .delete([authorize],deleteTeacher)
 
 
 module.exports = teacherRouter
